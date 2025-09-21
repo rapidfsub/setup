bd16749d3bc30c9e12257988b6571c0e7428a53e
Minsub Kim <rapidfsub@gmail.com>
2025-09-22 08:55:27 +0900

    seteup launchd

--- configuration.nix
+++ configuration.nix
@@ -46,4 +46,16 @@
     static-only = true;
     tilesize = 48;
   };
+
+  launchd.agents = {
+    linearMouse = {
+      command = "open -a LinearMouse.app";
+      serviceConfig.RunAtLoad = true;
+    };
+
+    postgres = {
+      command = "open -a Postgres.app";
+      serviceConfig.RunAtLoad = true;
+    };
+  };
 }
