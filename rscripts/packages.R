c9fcacef2044c3c317b0965b0980c388e9d7e263
Minsub Kim <rapidfsub@gmail.com>
2025-10-08 09:41:32 +0900

    add r taskfile

---
+++ rscripts/packages.R
@@ -0,0 +1,14 @@
+options(repos = c(CRAN = "https://ftp.kaist.ac.kr/CRAN/"))
+
+if (!requireNamespace("pak", quietly = TRUE)) {
+  install.packages("pak")
+}
+
+packages = c(
+  "ggplot2",
+  "languageserver",
+  "rmarkdown",
+  "purrr"
+)
+
+pak::pkg_install(packages)
