2c82f589a6315cd842a94204832f7a3485598d48
Minsub Kim <rapidfsub@gmail.com>
2025-10-15 10:39:03 +0900

    fix zcode fish function

--- home/.config/fish/functions/zcode.fish
+++ home/.config/fish/functions/zcode.fish
@@ -1,9 +1,10 @@
 function zcode
     argparse 'c/command=' -- $argv
+    set -l dst (zoxide query $argv 2>/dev/null)
 
-    begin
+    if test -d $dst
         pushd .
-        z $argv
+        cd $dst
         $_flag_command (ls | rg -m 1 '.code-workspace' || echo '.')
         popd
     end &>/dev/null
