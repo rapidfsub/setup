4c72a03a29f8144fe95104a97fc2337d850c4d58
Minsub Kim <rapidfsub@gmail.com>
2025-10-15 20:37:22 +0900

    fix vscode fish function for empty case

--- home/.config/fish/functions/zcode.fish
+++ home/.config/fish/functions/zcode.fish
@@ -2,10 +2,14 @@ function zcode
     argparse 'c/command=' -- $argv
     set -l dst (zoxide query $argv 2>/dev/null)
 
-    if test -d $dst
-        pushd .
-        cd $dst
-        $_flag_command (ls | rg -m 1 '.code-workspace' || echo '.')
-        popd
-    end &>/dev/null
+    if string match -q -r '\S' $dst
+        begin
+            pushd .
+            cd $dst
+            $_flag_command (ls | rg -m 1 '.code-workspace' || echo '.')
+            popd
+        end &>/dev/null
+    else
+        $_flag_command $argv
+    end
 end
