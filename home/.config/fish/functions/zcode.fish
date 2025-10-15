0236f82c63a9bd41821f450856740ab37e0d73d4
Minsub Kim <rapidfsub@gmail.com>
2025-10-16 08:50:48 +0900

    fix vscode fish function for current directory

--- home/.config/fish/functions/zcode.fish
+++ home/.config/fish/functions/zcode.fish
@@ -1,15 +1,11 @@
 function zcode
     argparse 'c/command=' -- $argv
-    set -l dst (zoxide query $argv 2>/dev/null)
 
-    if string match -q -r '\S' $dst
-        begin
-            pushd .
-            cd $dst
-            $_flag_command (ls | rg -m 1 '.code-workspace' || echo '.')
-            popd
-        end &>/dev/null
-    else
-        $_flag_command $argv
-    end
+    begin
+        pushd .
+        cd $argv
+        or z $argv
+        and $_flag_command (ls | rg -m 1 '.code-workspace' || echo '.')
+        popd
+    end &>/dev/null
 end
