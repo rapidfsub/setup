e20e0d3241f45f64413b589b763673d08ac1dc4e
Minsub Kim <rapidfsub@gmail.com>
2025-10-24 11:43:23 +0900

    chore

--- nix/flake.nix
+++ nix/flake.nix
@@ -40,6 +40,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.${user} = ./home.nix;
+              home-manager.backupFileExtension = "backup";
 
               # Optionally, use home-manager.extraSpecialArgs to pass
               # arguments to home.nix
