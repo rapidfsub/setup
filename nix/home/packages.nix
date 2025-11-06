19930d5a1220e9817169eaf6be9a8dba2f437159
Minsub Kim <rapidfsub@gmail.com>
2025-11-06 20:20:47 +0900

    update and fix lg

--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -12,6 +12,7 @@ in
 {
   home.packages = with pkgs; [
     chezmoi
+    lazygit
 
     # git
     git-delete-merged-branches
@@ -36,7 +37,7 @@ in
 
   programs.btop.enable = true;
   programs.direnv.enable = true;
-  programs.lazygit.enable = true;
+  # programs.lazygit.enable = true;
   programs.tealdeer.enable = true;
 
   programs.eza = enableFishIntegration true;
